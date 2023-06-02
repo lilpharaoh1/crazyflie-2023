7dcec90f9584b678a92b14daba30d69fa20c04e8
lilpharaoh1 <moustafe@tcd.ie>
2023-06-02 15:46:41 +0200

    Keeps bobbing up and down. I don't think it's updating fast enough

--- obs_course.py
+++ obs_course.py
@@ -16,10 +16,11 @@ from cflib.crazyflie.syncLogger import SyncLogger
 from cflib.utils import uri_helper
 
 URI = uri_helper.uri_from_env(default='radio://0/80/2M/E7E7E7E7E7')
-DEFAULT_VELOCITY = 0.2
-WAYPOINTS = np.transpose(np.array([[0.0,  0.0,  1.0],
-                                   [1.0,  1.0,  1.0],
-                                   [1.5,  1.3,  1.0]]))
+DEFAULT_VELOCITY = 0.75
+POINT_TOLERANCE = 0.2
+WAYPOINTS = np.array([[0.0,  0.0,  1.0],
+                      [1.0,  1.0,  1.0],
+                      [1.5,  1.3,  1.0]])
 
 
 class Drone: 
@@ -29,9 +30,8 @@ class Drone:
         self.pose = np.zeros((6, 1))
         self.drive = np.zeros((3, 1))
         self.waypoints = waypoints
-        self.obj = None if not len(self.waypoints) else self.waypoints[0]
+        self.obj = None if not len(self.waypoints) else self.waypoints[0].reshape((3,1))
         self.done = False
-        # self.scf = 
 
         self.logger = LogConfig(name='stateEstimate', period_in_ms=10)
         self.logger.add_variable('stateEstimate.x', 'float')
@@ -50,7 +50,7 @@ class Drone:
                 break
 
     def at(self, point):
-        if [abs(point[idx]-self.pose[idx, 0]) < 0.1 for idx in range(3)]:
+        if [abs(point[idx]-self.pose[idx, 0]) < POINT_TOLERANCE for idx in range(3)]:
             return True
         return False
     
@@ -58,22 +58,24 @@ class Drone:
         if not at:
             return self.obj
         np.delete(self.waypoints, 0, axis=0)
-        self.obj = None if not len(self.waypoints) else self.waypoints[0]
+        self.obj = None if not len(self.waypoints) else self.waypoints[0].reshape((3,1))
 
     def calc_obj_distance(self):
         return self.obj - self.pose[0:3]
 
     def spin(self):
         with SyncCrazyflie(URI, cf=Crazyflie(rw_cache='./cache')) as scf:
-            time.sleep(1)
-            while(not self.done):
-                self.log_pose(scf)
-                self.waypoint_select(self.at(self.obj))
-                with MotionCommander(scf, default_height=self.pose[2,0]) as mc:
-                    if self.obj == None:
-                            mc.land()
-                            time.sleep(3)
-                            break
+            with MotionCommander(scf, default_height=0.01) as mc:
+                while(not self.done):
+                    print("------------------------")
+                    self.log_pose(scf)
+                    self.waypoint_select(self.at(self.obj))
+                    print(self.pose[0:3], self.obj)
+                    if not isinstance(self.obj, np.ndarray):
+                        self.done = True
+                        mc.land()
+                        time.sleep(3)
+                        break
                     distance = self.calc_obj_distance()
                     mc.move_distance(distance[0,0], distance[1,0], distance[2,0], velocity=DEFAULT_VELOCITY)
 
--- test.py
+++ test.py
@@ -4,3 +4,10 @@ if arr:
     print("yes")
 else:
     print("no")
+
+print("---------------------------")
+
+import numpy as np 
+arr = np.array()
+
+print(np.zeros((3, 1)) )
